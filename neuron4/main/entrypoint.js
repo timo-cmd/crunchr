a550d2670449a08fc390c609089be5591d204ae6
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-05-17 11:16:06 +0200

    Create entrypoint.js

---
+++ neuron4/main/entrypoint.js
@@ -0,0 +1,20 @@
+/*   main entrypoint for neuron evaliation
+*    starts reading the interpreter memory-pattern of neuron1 - neuron3. 
+*    Then it will produce the ast and load the runtime.
+*    While a given input will be processed and evaluated, the binary memory-cell will be filled.
+*
+*
+*    Crunchr - A class-based language runtime that is implemented and evaluated using neuronal network patterns. 
+*    Submission project for the SIGMA compiler conference 2021
+*    Official research project lead by Timo Sarkar
+*    Built with love by Timo Sarkar and helpers
+*
+*
+*    Copyright 2020-2021 Timo Sarkar Crunchrlabs
+*/
+
+/*
+*
+*    Entrypoint file is still work in progress...
+*    Content will be soon added
+*/
